f327fb0a702d004c5abc776355128415b07867df
Rohit Kumar <rrohitkumar1077@gmail.com>
2026-05-16 19:42:53 +0530

    updated Resume

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -8,8 +8,9 @@ import HackerBtn from './animation/HackerBtn'
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-  
+      <a href="/Resume_rohit.pdf" download>
     <HackerBtn label='Download Resume' />
+    </a>
   </div>
   )
 }
